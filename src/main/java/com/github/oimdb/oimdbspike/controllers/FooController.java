02b3199de0a912134f6853ae2200d00e4a79a85c
Shahzada Hatim <hatim@hatimonline.com>
2012-09-12 20:07:37 +0200

    Added some more options in controllers

--- src/main/java/com/github/oimdb/oimdbspike/controllers/FooController.java
+++ src/main/java/com/github/oimdb/oimdbspike/controllers/FooController.java
@@ -4,11 +4,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
 
+import javax.ws.rs.POST;
+
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.github.oimdb.oimdbspike.domain.Meme;
 import com.github.oimdb.oimdbspike.domain.MemeMedium;
@@ -30,6 +33,11 @@ public class FooController {
     
     model.addAttribute("memeNames",memeRepository.getAllMemeNames());
     return "countPage"; // renders /WEB-INF/views/hello.jsp
+  }
+  
+  @RequestMapping(value="/input", method=RequestMethod.POST)
+  public String inputNewStuff(Model model) throws MalformedURLException {
+    return "input"; // renders /WEB-INF/views/hello.jsp
   }
   
   @RequestMapping("/bootstrap")
--- src/main/webapp/WEB-INF/views/countPage.jsp
+++ src/main/webapp/WEB-INF/views/countPage.jsp
@@ -11,6 +11,7 @@
 <title>Insert title here</title>
 </head>
 <body>
+
 <p> memeNames ${memeNames}</p>
 
 
